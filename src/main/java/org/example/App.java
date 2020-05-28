28a0f19168de680e588564eef03a904216bd7a9d
Francisco Sanchez <frango9000@gmail.com>
2020-05-28 12:43:17 +0200

    calculadora (operaciones no implementadas)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import org.example.Calculadora.Operacion;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Calculadora calculadora = new Calculadora(6, 3);
+        int suma = calculadora.calcular(Operacion.SUMA);
+        System.out.println("9: " + suma);
+        int resta = calculadora.calcular(Operacion.RESTA);
+        System.out.println("3: " + resta);
+        int mult = calculadora.calcular(Operacion.MULTIPLICACION);
+        System.out.println("18: " + mult);
+        int div = calculadora.calcular(Operacion.DIVISION);
+        System.out.println("2: " + div);
+
     }
 }
---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Calculadora {
+
+    private int num1 = 0;
+    private int num2 = 0;
+
+    public Calculadora() {
+    }
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public int calcular(Operacion operacion) {
+        int resultado = 0;
+        switch (operacion) {
+            case SUMA:
+                break;
+            case RESTA:
+                break;
+            case MULTIPLICACION:
+                break;
+            case DIVISION:
+                break;
+        }
+        return resultado;
+    }
+
+    public enum Operacion {
+        SUMA,
+        RESTA,
+        MULTIPLICACION,
+        DIVISION
+    }
+}
